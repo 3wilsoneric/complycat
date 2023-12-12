067e4aa9a393c2e6eb0bb021d28566016053ed72
3wilsoneric <54642091+3wilsoneric@users.noreply.github.com>
2023-12-12 09:53:40 +0000

    commit again 12/12/23

---
+++ aml_checker.py
@@ -0,0 +1,19 @@
+# complycat/aml_checker.py
+from typing import List
+
+class AMLChecker:
+    def __init__(self):
+        # Initialize the AML rules
+        self.aml_rules = []
+
+    def load_predefined_rules(self):
+        # Load predefined AML rules
+        pass
+
+    def add_custom_rule(self, rule):
+        # Add a custom AML rule
+        pass
+
+    def check_transaction(self, transaction_data):
+        # Perform AML compliance checks on the transaction data
+        pass
---
+++ document_stripper.py
@@ -0,0 +1,15 @@
+# complycat/document_stripper.py
+from typing import Dict, List
+
+class DocumentStripper:
+    def __init__(self):
+        # Initialize the document stripping rules
+        self.stripping_rules = {}
+
+    def load_stripping_rules(self):
+        # Load document stripping rules
+        pass
+
+    def strip_document(self, document_text):
+        # Strip text from the document based on predefined rules
+        pass
